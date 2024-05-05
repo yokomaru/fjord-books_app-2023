8e044400e0aa97fa199ae99d8f724da8b6d4352e
Junichi Ito <jit@sonicgarden.jp>
2024-05-05 11:28:27 +0900

    関連の名称をわかりやすい名前に改善

--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ReportMention < ApplicationRecord
-  belongs_to :mention_to, class_name: 'Report'
-  belongs_to :mentioned_by, class_name: 'Report'
+  belongs_to :mentioning, class_name: 'Report'
+  belongs_to :mentioned, class_name: 'Report'
 
-  validates :mentioned_by_id, uniqueness: { scope: :mention_to_id }
+  validates :mentioned_id, uniqueness: { scope: :mentioning_id }
 end
