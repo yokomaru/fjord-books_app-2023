ae6f1be5d68c3f7d31496945ef60e5951a4eaa8c
yokomaru <cabochetoganlan@gmail.com>
2024-06-21 23:30:28 +0900

    fix: order query string to symbol
    orderを文字列で書いていたがシンボルに書き直す

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    @books = Book.order('created_at DESC').page(params[:page])
+    @books = Book.order(created_at: :desc).page(params[:page])
   end
 
   # GET /books/1 or /books/1.json
