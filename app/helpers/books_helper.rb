afa4bc581aec5f5e21ba6d79b5c56d917103e5ab
yokomaru <cabochetoganlan@gmail.com>
2024-06-21 23:11:00 +0900

    fix: rubocop

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,7 +2,7 @@
 
 module BooksHelper
   def switch_language
-    if I18n.locale.to_s == "en"
+    if I18n.locale.to_s == 'en'
       link_to(t('views.common.japanese'), locale: 'ja')
     else
       link_to(t('views.common.english'), locale: 'en')
