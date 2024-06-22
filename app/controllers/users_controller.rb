8266ab916003aee29bfbfa192eb104572930c3df
yokomaru <cabochetoganlan@gmail.com>
2024-06-22 21:36:28 +0900

    add: before_action for access only login status
    ログイン状態のみアクセスできる画面の設定
    - books
    - users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @users = User.page(params[:page])
+    @users = User.order(:id).page(params[:page])
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 end
