6452b1d4b1f9600bbc9edad73d67730081b09b91
yokomaru <cabochetoganlan@gmail.com>
2024-06-22 21:35:45 +0900

    add: devise views and contollers
    以下のdevise viewとcontollerを追加
    - sessions
    - resisterations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+    }
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :books
   resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
