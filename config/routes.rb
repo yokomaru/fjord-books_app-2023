7d703f4fd7948b620217c3c3eb99bef6cfb9d8e9
yokomaru <cabochetoganlan@gmail.com>
2024-06-23 13:52:19 +0900

    fix: route.rb
    ハッシュロケットを:toに修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_out', to: 'devise/sessions#destroy'
   end
 
   resources :books
