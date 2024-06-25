593918c349bc65ac120adefb77b740cbad1232c7
yokomaru <cabochetoganlan@gmail.com>
2024-06-25 15:37:22 +0900

    delete: devise route
    各アクション後のパスはデフォルトで良いのでメソッドを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,16 +11,4 @@ class ApplicationController < ActionController::Base
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: %i[zipcode address introduction])
   end
-
-  def after_sign_up_path_for(*)
-    root_path
-  end
-
-  def after_sign_in_path_for(*)
-    books_path
-  end
-
-  def after_sign_out_path_for(*)
-    new_user_session_path
-  end
 end
