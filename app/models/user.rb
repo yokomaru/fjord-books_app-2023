bc60aa7870ae50383ff22abc3dd4643984e7f134
yokomaru <cabochetoganlan@gmail.com>
2024-06-28 09:28:13 +0900

    add: icon MVC
    プロフィール画像を追加するためのMVCを追加
    - User.rb
        - iconをアタッチ
        - 拡張子のバリデーション
        - リサイズ処理を追加(サムネイル、編集画面用プレビュー)
        - icon.attached?をメソッド化
     - ApplicationController
        - 画像更新や追加のためにStrongParameterにiconを追加
    - UsersController
         - N+1対策にwith_attached_iconを追加
    - View
        - iconを追加(編集、一覧、詳細)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :icon
+
+  validate :icon_type
+
+  has_one_attached :icon do |attachable|
+    attachable.variant :thumbnail, resize_to_limit: [200, 200]
+    attachable.variant :preview, resize_to_limit: [100, 100]
+  end
+
+  def icon_atacched?
+    icon.attached?
+  end
+
+  private
+
+  def icon_type
+    return if icon_atacched? && icon.blob.content_type.in?(%('image/jpeg image/png image/gif'))
+
+    errors.add(:icon, 'はjpegまたはpngまたはgif形式でアップロードしてください')
+  end
 end
