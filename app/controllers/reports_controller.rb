7dfd1a8a860e1f280169bccddf0b3e933e2d845c
yokomaru <cabochetoganlan@gmail.com>
2024-07-11 00:32:35 +0900

    add: save and update with mentions
    - 日報内に他の日報へ言及があった場合に追加する処理を追加
        - 日報への言及が更新されたら対応する
     - 更新対象の日報IDを取得するprivateメソッドを追加
    - contollerのメソッド変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,7 +21,7 @@ class ReportsController < ApplicationController
   def create
     @report = current_user.reports.new(report_params)
 
-    if @report.save
+    if @report.save_with_mentions
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
@@ -29,7 +29,7 @@ class ReportsController < ApplicationController
   end
 
   def update
-    if @report.update(report_params)
+    if @report.update_with_mentions(report_params)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
--- app/models/report.rb
+++ app/models/report.rb
@@ -18,4 +18,40 @@ class Report < ApplicationRecord
   def created_on
     created_at.to_date
   end
+
+  def save_with_mentions
+    Report.transaction do
+      save!
+      self.mentioning_report_ids = fetch_report_ids(content)
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    logger.error e.message
+    logger.error e.backtrace.join("\n")
+    errors.add(:report, 'の保存に失敗しました')
+    false
+  end
+
+  def update_with_mentions(params)
+    Report.transaction do
+      update!(params)
+      self.mentioning_report_ids = fetch_report_ids(content)
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    logger.error e.message
+    logger.error e.backtrace.join("\n")
+    errors.add(:report, 'の更新に失敗しました')
+    false
+  end
+
+  private
+
+  def fetch_report_ids(content)
+    registered_report_ids = Report.all.ids
+    report_ids = URI.extract(content).map do |url|
+      matched_report = url.match(%r{http://localhost:3000/reports/(\d+)(/|$)})
+      report_id = matched_report[1].to_i if matched_report.present?
+      report_id if registered_report_ids.include?(report_id)
+    end
+    report_ids.compact.uniq
+  end
 end
