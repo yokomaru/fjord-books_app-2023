deae5c2526d12d5412238825d6bca10fd78fdd1e
yokomaru <cabochetoganlan@gmail.com>
2024-07-12 10:20:27 +0900

    update: display time difference
    - 時差を表示するメソッドを経過時間を表示するメソッドという考え方に変えてメソッド名や変数名を変更
    - 各分岐の判断文や結果の処理をメソッド化
    - 時刻の数値を定数にする
    - i18n化

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,26 +1,42 @@
 # frozen_string_literal: true
 
 module ReportsHelper
+  ONE_MINUTE_SECONDS = 60
+  ONE_HOUR_SECONDS = 3600
+  ONE_DAY_SECONDS = 86_400
+  ONE_MONTH_SECONDS = 2_592_000
+  ONE_YEAR_SECONDS = 31_536_000
+
   def format_auto_link(content)
     Rinku.auto_link(format_content(content), :urls, 'target="_blank" rel="noopener noreferrer"', nil, Rinku::AUTOLINK_SHORT_DOMAINS)
   end
 
   # 現在の時刻と日報の作成日の差分を求め経過時間を表示
-  def display_time_difference_between_now_and_created_at(created_at)
-    time_difference = Time.zone.now - created_at
+  def display_passed_time(created_at)
+    passed_seconds = Time.zone.now - created_at
 
-    if time_difference < 60 # 秒
-      "#{time_difference.floor} seconds ago"
-    elsif time_difference > 60 && time_difference < 3600 # 分
-      "#{(time_difference / 60).floor} minutes ago"
-    elsif time_difference > 3600 && time_difference < 86_400 # 時
-      "#{(time_difference / 3600).floor} hour ago"
-    elsif time_difference > 86_400 && time_difference < 2_592_000 # 日
-      "#{(time_difference / 86_400).floor} day ago"
-    elsif time_difference > 2_592_000 && time_difference < 31_536_000 # 月
-      "#{(time_difference / 2_592_000).floor} month ago"
-    elsif time_difference > 31_536_000 # 月
-      "#{(time_difference / 31_536_000).floor} year ago"
+    if passed_seconds < ONE_MINUTE_SECONDS
+      "#{passed_seconds.floor} #{t('.seconds_ago')}" # i18nと定数化
+    elsif seconds_between_min_and_max_time_units?(passed_seconds, ONE_MINUTE_SECONDS, ONE_HOUR_SECONDS)
+      "#{calculate_seconds_to_time_unit(passed_seconds, ONE_MINUTE_SECONDS)} #{t('.minutes_ago')}"
+    elsif seconds_between_min_and_max_time_units?(passed_seconds, ONE_HOUR_SECONDS, ONE_DAY_SECONDS)
+      "#{calculate_seconds_to_time_unit(passed_seconds, ONE_HOUR_SECONDS)} #{t('.hours_ago')}"
+    elsif seconds_between_min_and_max_time_units?(passed_seconds, ONE_DAY_SECONDS, ONE_MONTH_SECONDS)
+      "#{calculate_seconds_to_time_unit(passed_seconds, ONE_DAY_SECONDS)} #{t('.days_ago')}"
+    elsif seconds_between_min_and_max_time_units?(passed_seconds, ONE_MONTH_SECONDS, ONE_YEAR_SECONDS)
+      "#{calculate_seconds_to_time_unit(passed_seconds, ONE_MONTH_SECONDS)} #{t('.months_ago')}"
+    elsif passed_seconds > ONE_YEAR_SECONDS
+      "#{calculate_seconds_to_time_unit(passed_seconds, ONE_YEAR_SECONDS)} #{t('.years_ago')}"
     end
+  end
+
+  private
+
+  def seconds_between_min_and_max_time_units?(passed_seconds, min_time_unit_seconds, max_time_unit_seconds)
+    (min_time_unit_seconds..max_time_unit_seconds).cover?(passed_seconds)
+  end
+
+  def calculate_seconds_to_time_unit(passed_seconds, unit_seconds)
+    (passed_seconds / unit_seconds).floor
   end
 end
