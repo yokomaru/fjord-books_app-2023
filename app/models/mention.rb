b6763af03188c66fd6733aa39f4b5fb6962533ad
yokomaru <cabochetoganlan@gmail.com>
2024-07-14 22:03:01 +0900

    update: add unique relation to mention
    - メンションにreport_idとmentioned_report_idの組み合わせを一位にするバリデーションを追加

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,6 @@
 class Mention < ApplicationRecord
   belongs_to :report
   belongs_to :mentioned_report, class_name: 'Report'
+
+  validates :report_id, uniqueness: { scope: :mentioned_report_id }
 end
