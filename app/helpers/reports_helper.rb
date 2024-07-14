8dda464a65ba5a20478d3f4ade69963abe2f36c2
yokomaru <cabochetoganlan@gmail.com>
2024-07-14 22:00:41 +0900

    update: pluralize time unit
    - 時間の単位の複数形を対応
        - Qiitaのように時間経過の単位は英語表記のみ
        - 上記をメソッド化
        - 不要になったi18n対応を削除

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -16,17 +16,17 @@ module ReportsHelper
     passed_seconds = Time.zone.now - created_at
 
     if passed_seconds < ONE_MINUTE_SECONDS
-      "#{passed_seconds.floor} #{t('.seconds_ago')}" # i18nと定数化
+      passed_time_pluralize(passed_seconds.floor, 'second').to_s
     elsif seconds_between_min_and_max_time_units?(passed_seconds, ONE_MINUTE_SECONDS, ONE_HOUR_SECONDS)
-      "#{calculate_seconds_to_time_unit(passed_seconds, ONE_MINUTE_SECONDS)} #{t('.minutes_ago')}"
+      passed_time_pluralize(calculate_seconds_to_time_unit(passed_seconds, ONE_MINUTE_SECONDS), 'minute').to_s
     elsif seconds_between_min_and_max_time_units?(passed_seconds, ONE_HOUR_SECONDS, ONE_DAY_SECONDS)
-      "#{calculate_seconds_to_time_unit(passed_seconds, ONE_HOUR_SECONDS)} #{t('.hours_ago')}"
+      passed_time_pluralize(calculate_seconds_to_time_unit(passed_seconds, ONE_HOUR_SECONDS), 'hour').to_s
     elsif seconds_between_min_and_max_time_units?(passed_seconds, ONE_DAY_SECONDS, ONE_MONTH_SECONDS)
-      "#{calculate_seconds_to_time_unit(passed_seconds, ONE_DAY_SECONDS)} #{t('.days_ago')}"
+      passed_time_pluralize(calculate_seconds_to_time_unit(passed_seconds, ONE_DAY_SECONDS), 'day').to_s
     elsif seconds_between_min_and_max_time_units?(passed_seconds, ONE_MONTH_SECONDS, ONE_YEAR_SECONDS)
-      "#{calculate_seconds_to_time_unit(passed_seconds, ONE_MONTH_SECONDS)} #{t('.months_ago')}"
+      passed_time_pluralize(calculate_seconds_to_time_unit(passed_seconds, ONE_MONTH_SECONDS), 'month').to_s
     elsif passed_seconds > ONE_YEAR_SECONDS
-      "#{calculate_seconds_to_time_unit(passed_seconds, ONE_YEAR_SECONDS)} #{t('.years_ago')}"
+      passed_time_pluralize(calculate_seconds_to_time_unit(passed_seconds, ONE_YEAR_SECONDS), 'year').to_s
     end
   end
 
@@ -38,5 +38,9 @@ module ReportsHelper
 
   def calculate_seconds_to_time_unit(passed_seconds, unit_seconds)
     (passed_seconds / unit_seconds).floor
+  end
+
+  def passed_time_pluralize(time, time_unit)
+    "#{time} #{time_unit.pluralize(time)} ago"
   end
 end
