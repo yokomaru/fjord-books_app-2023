382f1d90903f23339eb40f879698202114473ecb
yokomaru <cabochetoganlan@gmail.com>
2024-08-10 23:04:46 +0900

    add: Report model test
    - インスタンスメソッドのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,21 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    user = User.new(email: 'foo@example.com', name: 'Foo')
+
+    report = user.reports.new(title: 'title', content: 'content')
+
+    assert report.editable?(user)
+
+    other_user = User.new(email: 'bar@example.com', name: 'Bar')
+
+    assert_not report.editable?(other_user)
+  end
+
+  test '#created_on' do
+    user = User.create!(email: 'foo@example.com', name: 'Foo', password: 'password')
+    report = user.reports.create!(title: 'title', content: 'content')
+    assert_equal Time.now.to_date, report.created_on
+  end
 end
